7a3d308205b20aa4dcca10cfb67ec9133143552f
Frankie Tu <hua.tu@hotmail.com>
2019-05-15 21:51:01 -0400

    added mario.c and README.md

---
+++ week1/mario.c
@@ -0,0 +1,46 @@
+#include <cs50.h>
+#include <stdio.h>
+
+void mario(void);
+void loop_print(char symbol, int times);
+
+/*
+ * print mario pyramid
+ * enter height:5
+ *     #  #    
+ *    ##  ##   
+ *   ###  ###  
+ *  ####  ####  
+ * #####  #####
+ */
+int main(void)
+{
+    mario();
+}
+
+void mario(void)
+{
+    int height;
+    do
+    {    
+        height = get_int("Height: ");
+    }
+    while (height < 1 || height > 8);
+    
+    for (int x = 1; x <= height; x++)
+    { 
+       loop_print(' ', height - x);
+       loop_print('#', x);
+       printf("  ");
+       loop_print('#', x);
+       printf("\n");
+    }
+}
+
+void loop_print(char symbol, int times)
+{
+    for (int x = 0; x < times; x++)
+    {
+        printf("%c", symbol);
+    }
+}
